dba5f6fb466ef4125f1f2ca7f31b568003488853
Zaid Ismail <zaidismail031@gmail.com>
2023-12-27 17:40:55 +0200

    Reorganize directories

--- make.bat
+++ make.bat
@@ -1,2 +1,6 @@
-ca65 hellomario.asm -o hellomario.o --debug-info
+@echo off
+mkdir build
+pushd build
+ca65 ..\source\hellomario.asm -o hellomario.o --debug-info
 ld65 hellomario.o -o hellomario.nes -t nes --dbgfile hellomario.dbg
+popd
